bd564aef118a0185d686575c669ae73c4cc77ac6
pukmajster <45610204+pukmajster@users.noreply.github.com>
2021-03-12 16:28:23 +0100

    removed junk, added run shortcut

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
 import express from 'express';
 import notifier from 'node-notifier';
 import open from 'open'
-import cors from 'cors'
-import fs from 'fs'
-import https from 'https'
+import path from 'path'
 
 const app = express();
 const port = 3000;
+
+
+function notify({title, message, ...rest}, func = null) {
+    notifier.notify({
+        title,
+        message,
+        icon: 'C:/Users/pukis/Desktop/crosslink-api/public/crosslink.png',
+        // contentIcon: 'C:/Users/pukis/Desktop/crosslink-api/public/crosslink.png',
+        // icon: path.join(__dir, '/public/crosslink.png'),
+        // contentIcon: path.join(__dirname, '/public/crosslink.png'),
+        // sound: false,
+        subtitle: 'yeet',
+        // wait: false,
+        // appID: 'crosslink-api',
+        ...rest
+    }, func)
+}
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -20,20 +35,24 @@ app.use(function(req, res, next) {
 app.get('/open-link-on-pc', (req, res) => {
 
     // Make sure we have all the query params we need
+
+    console.log(req.query);
+    
+
     const { title, url } = req.query;
     if(!title || !url) {
         return res.status(400).send('Invalid parameters');
     }
 
     // Ask the user if they want to open the link
-    notifier.notify({
-        title: 'Open link?',
-        subtitle: title,
-        message: title,
-        sound: false,
-        wait: false
+    notify({
+        title: 'Click to open:',
+        message: title
     },
     async function (err, response, metadata) {
+
+        console.log(metadata.action);
+        
 
         // If the user clicked on the notifiaction, open the url
         if(metadata.action == 'clicked') {
@@ -59,12 +78,11 @@ app.use(function (req, res) {
 
 app.listen(port, () => {
 
-    // notifier.notify({
-    //     title: 'crosslink-api live!',
-    //     message: `crosslink-api is listening on ${port}`,
-    //     sound: false,
-    //     wait: false
-    // });
+    notify({
+        title: 'crosslink-api live!',
+        message: `crosslink-api is listening on ${port}`,
+    });
+    console.log(path.join(__dirname, '../public/crosslink.png'));
 
     console.log(`server is listening on ${port}`);
 });
